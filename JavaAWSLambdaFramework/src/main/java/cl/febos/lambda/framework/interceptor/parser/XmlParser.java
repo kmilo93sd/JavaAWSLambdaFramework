ba1f80fa777a2d427fa36a538543e1fa419e1940
Camilo Alaniz <kmilo93s@gmail.com>
2018-06-25 16:48:38 -0400

    Interface XmlParser
    Hay que implementar una librería

---
+++ JavaAWSLambdaFramework/src/main/java/cl/febos/lambda/framework/interceptor/parser/XmlParser.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.febos.lambda.framework.interceptor.parser;
+
+import cl.febos.lambda.framework.lambda.BaseRequest;
+
+
+/**
+ *
+ * @author Camilo
+ */
+public interface XmlParser {
+    public BaseRequest getRequestObjectFromXml(String request, BaseRequest requestClass);
+    public boolean isXml(String xml);
+}
