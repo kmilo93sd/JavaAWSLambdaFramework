b247e583dbebec4bbfca9627b6bb788d85d1d191
Camilo Alaniz <kmilo93s@gmail.com>
2018-06-25 16:49:13 -0400

    Creada clase RequestParser
    Encargada de cualquier parseo necesario para el request y response

---
+++ JavaAWSLambdaFramework/src/main/java/cl/febos/lambda/framework/interceptor/parser/RequestParser.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.febos.lambda.framework.interceptor.parser;
+
+import cl.febos.lambda.framework.lambda.BaseRequest;
+import cl.febos.lambda.framework.lambda.BaseResponse;
+
+
+/**
+ *
+ * @author Camilo
+ */
+public class RequestParser {
+    
+    private XmlParser xmlParser;
+    private JsonParser jsonParser;
+    
+    public RequestParser(XmlParser xmlParser, JsonParser jsonParser){
+        this.xmlParser = xmlParser;
+        this.jsonParser = jsonParser;
+    }
+    
+    public BaseRequest getObjectFromStringRequest(String request, BaseRequest requestClass){
+        
+        if(isJsonString(request)){
+            return jsonParser.getRequestObjectFromJson(request, requestClass);
+        }
+        
+        if(isXmlString(request)){
+            return xmlParser.getRequestObjectFromXml(request, requestClass);
+        }
+        
+        return null;//checkThis
+    }
+    
+    private boolean isJsonString(String request){
+        
+        return jsonParser.isJson(request);
+    }
+    
+    private boolean isXmlString(String request) {
+        return xmlParser.isXml(request);
+    }
+    
+    public String getJsonFromBaseResponse(BaseResponse baseResponse){
+        
+        return jsonParser.getJsonFromBaseResponse(baseResponse);
+    }
+
+    
+    
+}
